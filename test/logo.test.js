fe4e85b074384e7ee582d664fa8ef25300fb2273
mackemo <mackenzielmoore14@gmail.com>
2024-05-25 17:36:51 -0400

    included the tests for the three other prompts

--- test/logo.test.js
+++ test/logo.test.js
@@ -7,7 +7,32 @@ describe('Logo', () => {
         it('should have a string with three charcters or less', () => {
             expect(logoText('abc')).toEqual(result);
         });
+        it('should include only alphanumeric characters', () => {
+            expect(logoText('1ab')).toEqual(result);
+        });
     });
 
-    describe('textColor')
+    describe('textColor', () => {
+        const textColor = inquirer.prompt[1].validate;
+        const result = true;
+        it('should have a valid color keyword', () => {
+            expect(textColor('white')).toEqual(result);
+        });
+    });
+
+    describe('logoShape', () => {
+        const logoShape = inquirer.prompt[2].validate;
+        const result = true;
+        it('should include a shape from the choices', () => {
+            expect(logoShape('circle')).toEqual(result);
+        });
+    });
+
+    describe('shapeColor', () => {
+        const shapeColor = inquirer.prompt[3].validate;
+        const result = true;
+        it('should have a valid color keyword', () => {
+            expect(shapeColor('blue')).toEqual(result);
+        });
+    });
 })
