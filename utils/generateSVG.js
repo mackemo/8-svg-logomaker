707fe77ed6cb144f89bc5b53a467c901d502c008
mackemo <mackenzielmoore14@gmail.com>
2024-05-26 10:11:24 -0400

    imported Logo object, changed variables to match logo object data in generateSVG file

--- utils/generateSVG.js
+++ utils/generateSVG.js
@@ -1,3 +1,5 @@
+const Logo = require('../lib/logo')
+
 function generateSVG () {
     let svg = '';
 
@@ -9,20 +11,21 @@ function generateSVG () {
     svg += `<svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">`;
 
     // logo shape and color
-    if (logo.logoShape === 'circle') {
-        svg += `<circle cx="${width / 2}" cy="${height / 2}" r="${Math.min(width, height) / 2}" fill="${logo.shapeColor}" />`;
-    } else if (logo.logoShape === 'square') {
-        svg += `<rect x="0" y="0" width="${width}" height="${height}" fill="${logo.shapeColor}" />`;
-    } else if (logo.logoShape === 'triangle') {
+    if (Logo.logoShape === 'circle') {
+        svg += `<circle cx="${width / 2}" cy="${height / 2}" r="${Math.min(width, height) / 2}" fill="${Logo.shapeColor}" />`;
+    } else if (Logo.logoShape === 'square') {
+        svg += `<rect x="0" y="0" width="${width}" height="${height}" fill="${Logo.shapeColor}" />`;
+    } else if (Logo.logoShape === 'triangle') {
         const points = `0,${height} ${width},${height} ${width / 2},0`;
-        svg += `<polygon points="${points}" fill="${logo.shapeColor}" />`;
+        svg += `<polygon points="${points}" fill="${Logo.shapeColor}" />`;
     }
 
     // logo text and color
-    svg += `<text x="50%" y="50%" fill="${logo.textColor}" text-anchor="middle">${logo.logoText}</text>`;
+    svg += `<text x="50%" y="50%" fill="${Logo.textColor}" text-anchor="middle">${Logo.logoText}</text>`;
 
     svg += `</svg>`;
 
     return svg;
 }
+
 module.exports = generateSVG;
